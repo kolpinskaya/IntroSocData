e16512ece6e4302184eee1fd13295aee21288cc3
Alexey <bessudnov@gmail.com>
2016-07-07 10:19:16 +0100

    first draft

---
+++ lesson1/initLesson.R
@@ -0,0 +1,10 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+music <- data.frame(name = c("Adele", "Madonna", "Rihanna", "Drake", "Eminem", "Skrillex"),
+		gender = c(rep(1, 3), rep(0, 3)),
+		no_album = c(5, 13, 8, 4, 8, 1),
+		no_awards = c(105, 310, 125, 38, 155, 11),
+		no_tours = c(3, 10, 6, 4, 7, NA))
